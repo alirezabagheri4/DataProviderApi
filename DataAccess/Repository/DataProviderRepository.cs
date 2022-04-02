c3b5eebbace4b5e618b251c578a034815bab4b70
a.bagheri <fhw34514@gmail.com>
2022-04-03 00:38:17 +0430

    hhh

--- DataAccess/Repository/DataProviderRepository.cs
+++ DataAccess/Repository/DataProviderRepository.cs
@@ -1,22 +1,109 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using DataAccess.Tools;
+﻿using DataAccess.Tools;
 using DomainModel.DataModel;
 using DomainModel.Interface.DataProviderRepository;
-using Microsoft.Extensions.Configuration;
 
 namespace DataAccess.Repository
 {
     public class DataProviderRepository : IDataProviderRepository
     {
         private readonly ISqlDataAccessDapper _accessDapper;
-        public DataProviderRepository( ISqlDataAccessDapper accessDapper)
+        public DataProviderRepository(ISqlDataAccessDapper accessDapper)
         {
             _accessDapper = accessDapper;
+        }
+
+        public List<Task> AddBach(List<DynamicObjectDO> t)
+        {
+            var className = t.FirstOrDefault()?.ClassNameAndId.Item1;
+            var record = t.GroupBy(x => x.ClassNameAndId.Item2)
+                .Select(c => new { c }).ToList();
+
+            var result = new List<Task>();
+            try
+            {
+                string useDB = "use DataProvider ";
+                string ifStatement = $@"
+                IF (EXISTS (SELECT * 
+                FROM INFORMATION_SCHEMA.TABLES
+                WHERE TABLE_SCHEMA = 'dbo'
+                AND TABLE_NAME = '{className}'))";
+                string begin = "Begin ";
+
+                string? insrt1 = " "; string? property1 = " "; string? value1 = null;
+
+                foreach (var item in record)
+                {
+                    insrt1 += $"Insert Into [dbo].[{className}]";
+                    property1 += "(";
+                    foreach (var prop in item.c.ToList())
+                    {
+                        property1 += $"[{prop.PropertyName}],";
+                    }
+                    property1 += "[SubmitDate] ) ";
+                    value1 = "VALUES (";
+                    foreach (var value in item.c.ToList())
+                    {
+                        value1 += $" N'{value.PropertyValue}' ,";
+                    }
+                    value1 += $" '{DateTime.Now}' ) ";
+                }
+
+                string endIf = "END ";
+                string elseStr = "ELSE BEGIN ";
+                string? soton = null;
+                string? creatTable = $"CREATE TABLE [dbo].[{className}] (";
+                string? insert2 = ""; string? property2 = ""; string? value2 = null;
+                foreach (var rec in record)
+                {
+                    foreach (var prop in rec.c.ToList())
+                    {
+                        if (prop.PropertyName.ToUpper().Equals("ID"))
+                        {
+                            soton += "Id INT  PRIMARY KEY ,";
+                        }
+                        else
+                        {
+                            soton += $"[{prop.PropertyName}] Nvarchar(50) ,";
+                        }
+                    }
+                    break;
+                }
+
+                soton += "[SubmitDate] [datetime2](3) NOT NULL )";
+
+                foreach (var item in record)
+                {
+                    insert2 += $"Insert Into [dbo].[{className}]";
+                    property2 += "(";
+                    foreach (var prop in item.c.ToList())
+                    {
+                        property2 += $"[{prop.PropertyName}],";
+                    }
+                    property2 += "[SubmitDate] )";
+                    value2 = "VALUES (";
+                    foreach (var value in item.c.ToList())
+                    {
+                        value2 += $" N'{value.PropertyValue}' ,";
+                    }
+                    value2 += $" '{DateTime.Now}' ) ";
+                }
+                string endElse = "END ";
+                string query = useDB + ifStatement + begin
+                               + insrt1 + property1 + value1 + endIf +
+                               elseStr + creatTable + soton +
+                               insert2 + property2 + value2 + endElse;
+
+                foreach (var o in t)
+                {
+                    result.Add(_accessDapper.SaveData(query, new { }));
+                }
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         async Task<dynamic> IDataProviderRepository.Get(int id)
@@ -67,100 +154,6 @@ namespace DataAccess.Repository
             try
             {
                 return _accessDapper.SaveData("dbo.spUser_Update", id);
-            }
-            catch (Exception e)
-            {
-                throw new Exception(e.Message);
-            }
-        }
-
-        public List<Task> AddBach(List<DynamicObjectDO> t)
-        {
-            var className = t.FirstOrDefault()?.ClassNameAndId.Item1;
-            var record = t.GroupBy(x => x.ClassNameAndId.Item2).Select(c=>new {c}).ToList();
-
-            var result= new List<Task>();
-            try
-            {
-                string useDB = "use DataProvider";
-                string ifStatement = $@"IF (EXISTS (SELECT * 
-                FROM INFORMATION_SCHEMA.TABLES
-                WHERE TABLE_SCHEMA = 'dbo'
-                AND TABLE_NAME = '{className}'))";
-                string begin = "Begin";
-
-                string? insrt1 = null; string? property1 = null; string? value1 = null;
-                foreach (var item in record)
-                {
-                    insrt1= $"Insert Into {className}";
-                     property1 = "(";
-                    foreach (var prop in item.c.ToList())
-                    {
-                        property1+= $"{prop.PropertyName},";
-                    }
-                    property1.Remove(property1.Length - 1);
-                    property1 += ")";
-                    value1 = "VALUES (";
-                    foreach (var value in item.c.ToList())
-                    {
-                        value1 += $"{value.PropertyValue},";
-                    }
-                    value1.Remove(value1.Length - 1);
-                    value1 += ") ";
-                }
-
-                string endIf = "END";
-                string elseStr = "ELSE BEGIN";
-
-                string? insert2 = null; string? property2 = null; string? value2 = null;
-                string? soton = null;
-                string? creatTable= $"CREATE TABLE [dbo].[{className}] (";
-                foreach (var rec in record)
-                {
-                    foreach (var prop in rec.c.ToList())
-                    {
-                        if (prop.PropertyName.ToUpper().Equals("ID"))
-                        {
-                            soton += "Id INT  PRIMARY KEY ,";
-                        }
-                        else
-                        {
-                            soton += $"{prop.PropertyName} Nvarchar(50) ,";
-                        }
-                    }
-                }
-
-                soton += "[SubmitDate] [datetime2](3) NOT NULL )";
-
-                foreach (var item in record)
-                {
-                    insert2 = $"Insert Into {className}";
-                    property2 = "(";
-                    foreach (var prop in item.c.ToList())
-                    {
-                        property2 += $"{prop.PropertyName},";
-                    }
-                    property2.Remove(property2.Length - 1);
-                    property2 += ")";
-                    value2 = "VALUES (";
-                    foreach (var value in item.c.ToList())
-                    {
-                        value2 += $"{value.PropertyValue},";
-                    }
-                    value2.Remove(value2.Length - 1);
-                    value2 += ") ";
-                }
-
-                string query = useDB + ifStatement + begin + insrt1 + property1 + value1 + endIf +
-                               elseStr + insert2 + property2 + value2;
-
-                foreach (var o in t)
-                {
-                    result.Add(_accessDapper.SaveData
-                        ("dbo.spUser_Insert", new {Firstname = o.GetType(), Lastname = "user.LastName"}));
-                }
-
-                return result;
             }
             catch (Exception e)
             {
